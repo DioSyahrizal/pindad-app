b3a9388980e49afb97367600d2a1ddb96343b36c
diosyahrizal <diosyahrizal@gmail.com>
2023-07-15 19:33:02 +0700

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+<x-layout>
+    <div class="login-wrapper">
+        <div class="row">
+            <div class="col-12">
+                <h1>Home</h1>
+            </div>
+        </div>
+    </div>
+</x-layout>

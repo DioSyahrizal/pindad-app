6f280d055c68116a9330c6003b9df74ecc9eb2cd
Dio Syahrizal <diosyahrizal@gmail.com>
2023-07-25 16:33:49 +0700

    feat: initiate wizard form

---
+++ app/Http/Livewire/Mu5tjWizard.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Mu5tjWizard extends Component
+{
+    public function render()
+    {
+        return view('livewire.mu5tj-wizard');
+    }
+}

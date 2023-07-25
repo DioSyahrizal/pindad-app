60185ef8f3525163d667a2ab2371737bb2cf6685
Dio Syahrizal <diosyahrizal@gmail.com>
2023-07-25 22:23:03 +0700

    fix: fix css import

--- resources/views/components/auth-layout.blade.php
+++ resources/views/components/auth-layout.blade.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite('resources/css/app.css')
-    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    @vite('resources/css/styles.css')
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.5/css/jquery.dataTables.css"/>
